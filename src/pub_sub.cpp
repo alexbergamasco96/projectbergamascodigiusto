0ceeba81d12e3995440ee494da0857066291f510
alexbergamasco96 <alex.bergamasco@mail.polimi.it>
2019-05-17 16:52:47 +0200

    add custom msg

--- src/pub_sub.cpp
+++ src/pub_sub.cpp
@@ -1,58 +1,62 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "projectbergamascodigiusto/floatStamped.h"
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
 
 
-class pub_sub
-{
+class pub_sub{
 
-std_msgs::String messagio;
-std_msgs::String messagio2;
+	std_msgs::String messagio;
+	std_msgs::String messagio2;
 
-private:
-ros::NodeHandle n; 
+	private:
+	ros::NodeHandle n; 
 
-ros::Subscriber sub;
-ros::Subscriber sub2;
-ros::Publisher pub; 
-ros::Timer timer1;
+	/*ros::Subscriber sub;
+	ros::Subscriber sub2;*/
+	message_filters::Subscriber<projectbergamascodigiusto::floatStamped> subLeft(n, "speedL_stamped", 1);
+	message_filters::Subscriber<projectbergamascodigiusto::floatStamped> subRight(n, "speedR_stamped", 1);
+	message_filters::Subscriber<projectbergamascodigiusto::floatStamped> subSteer(n, "steer_stamped", 1);
+	message_filters::TimeSynchronizer<projectbergamascodigiusto::floatStamped, projectbergamascodigiusto::floatStamped, projectbergamascodigiusto::floatStamped> sync(subLeft, subRight, subSteer, 10);
 	
-	
-public:
-  	pub_sub(){
-  	sub = n.subscribe("/chatter", 1, &pub_sub::callback, this);
-	sub2 = n.subscribe("/chatter2", 1, &pub_sub::callback2, this);
-	pub = n.advertise<std_msgs::String>("/rechatter", 1);
-	timer1 = n.createTimer(ros::Duration(1), &pub_sub::callback1, this);
-	
+	//idea: if function. if(odom=diff){sync.reg ... with callback1} else ...callback2
+	//callback1 compute differential drive, callback2 ackermann
+	sync.registerCallback(boost::bind(&callback, _1, _2, _3));
 
-}
-void callback(const std_msgs::String::ConstPtr& msg){
-messagio=*msg;
-
-}
-
-void callback2(const std_msgs::String::ConstPtr& msg){
-messagio2=*msg;
-
-}
-
-void callback1(const ros::TimerEvent&)
-{
-	pub.publish(messagio);
-	pub.publish(messagio2);
-  	ROS_INFO("Callback 1 triggered");
-}
+	ros::Publisher pub; 
+	ros::Timer timer1;
+		
+		
+	public:
+	pub_sub(){
+		
+		sub = n.subscribe("/chatter", 1, &pub_sub::callback, this);
+		sub2 = n.subscribe("/chatter2", 1, &pub_sub::callback2, this);
+		pub = n.advertise<std_msgs::String>("/rechatter", 1);
+		timer1 = n.createTimer(ros::Duration(1), &pub_sub::callback1, this);
+		
+	}
 
 
 
-
+	//Here compute the odometry.
+	void callback(const std_msgs::String::ConstPtr& msg){
+		messagio=*msg;
+	}
 
 };
 
-int main(int argc, char **argv)
-{
+
+
+
+//Classic main of pub_sub in the same node
+
+int main(int argc, char **argv){
+
  	ros::init(argc, argv, "subscribe_and_publish");
  	pub_sub my_pub_sub;
  	ros::spin();
  	return 0;
+
 }
