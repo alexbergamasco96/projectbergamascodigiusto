71f960eff2a11722bbff775eab6104593d8afcb1
Federico <federico.digiusto@mail.polimi.it>
2019-05-17 21:08:50 +0200

    lll

--- src/pub_sub.cpp
+++ src/pub_sub.cpp
@@ -9,6 +9,10 @@
 
 class pub_sub{
 
+	projectbergamascodigiusto::floatStamped left1;
+	projectbergamascodigiusto::floatStamped right1;
+	projectbergamascodigiusto::floatStamped steer1;
+
 	private:
 	ros::NodeHandle n; 
 			
@@ -21,7 +25,7 @@ class pub_sub{
 		typedef message_filters::sync_policies::ApproximateTime<projectbergamascodigiusto::floatStamped, projectbergamascodigiusto::floatStamped, projectbergamascodigiusto::floatStamped> MySyncPolicy;
 
   		message_filters::Synchronizer<MySyncPolicy> sync(MySyncPolicy(10), subLeft, subRight,subSteer);
-	    sync.registerCallback(boost::bind(callback, _1, _2));
+	    sync.registerCallback(boost::bind(&pub_sub::callback,this, _1, _2,_3));
 
 		//idea: if function. if(odom=diff){sync.reg ... with callback1} else ...callback2
 		//callback1 compute differential drive, callback2 ackermann
@@ -32,9 +36,11 @@ class pub_sub{
 
 
 	//Here compute the odometry.
-	void callback(const projectbergamascodigiusto::floatStamped& left, const projectbergamascodigiusto::floatStamped& right, const projectbergamascodigiusto::floatStamped& steer){
-
-		ROS_INFO ("Received a messages: (%f,%f,%f)", left.data  , right.data, steer.data);
+	void callback(const projectbergamascodigiusto::floatStamped::ConstPtr& left, const projectbergamascodigiusto::floatStamped::ConstPtr& right, const projectbergamascodigiusto::floatStamped::ConstPtr& steer){
+		left1=*left;
+		right1=*right;
+		steer1=*steer;
+		ROS_INFO ("Received a messages: (%f,%f,%f)", left1.data  , right1.data, steer1.data);
 
 	}
 
