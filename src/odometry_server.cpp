fd4931e39760ea443431956398fb93d051f552c4
Federico <federico.digiusto@mail.polimi.it>
2019-05-19 17:58:01 +0200

    euler fixed

--- src/odometry_server.cpp
+++ src/odometry_server.cpp
@@ -15,29 +15,32 @@ int count;
 
 
 bool odometryComputation(projectbergamascodigiusto::OdometryComputation::Request &req,projectbergamascodigiusto::OdometryComputation::Response &res){
-    ROS_INFO("[SERVER]  Request: vRight=%f,vLeft=%f,steerSensor=%f",(double)req.speedR,(double)req.speedL,(double)req.steer_sensor);
+    ROS_INFO("[SERVER]  Request: vLeft=%f,vRight=%f,steerSensor=%f",(double)req.speedL,(double)req.speedR,(double)req.steer_sensor);
     ROS_INFO("[TIME SERVER] time=%f",(double)req.seconds);
 
     double l=0.130;
     if(count==0){
         x_before=0;
-        y_before=0;    
+        y_before=0;
         theta_before=0;
     }
 
     double v_k=0.5*((double)req.speedL+(double)req.speedR); //velocity
     double t_s=(double)req.seconds-time_before; //tempo campionamento 
     double w_k=((double)req.speedL+(double)req.speedR)/l;
+
     
     //Euler
     x_comput=x_before+v_k*t_s*cos(theta_before);
     y_comput=y_before+v_k*t_s*sin(theta_before);
     theta_comput=theta_before+w_k*t_s;
 
-    ROS_INFO("[SERVER]  position: (x,y)=(%f,%f) theta=%f",x_comput,y_comput,theta_comput);
+    ROS_INFO("[SERVER]  position: (x,y)=(%f,%f) theta=%f ",x_comput,y_comput,theta_comput);
+    ROS_INFO("[SERVER] INFO: Ts=%f, w=%f, v=%f", t_s,w_k,v_k);
 
     x_before=x_comput;
     y_before=x_comput;
+    time_before=(double)req.seconds;
     theta_before=theta_comput;
     count++;
 
