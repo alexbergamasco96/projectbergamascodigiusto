0a1cdf03303885a651d80c9c9b6280dc819fe4a1
alexbergamasco96 <alex.bergamasco@mail.polimi.it>
2019-05-23 15:32:33 +0200

    Add ackerman steering and Runge-Kutta approx.

--- src/odometry_server.cpp
+++ src/odometry_server.cpp
@@ -1,8 +1,9 @@
 #include "ros/ros.h"
 #include <cmath> 
 #include "projectbergamascodigiusto/OdometryComputation.h"
-double time_before;
 
+
+double time_before;
 double x_before;
 double y_before;
 double theta_before;
@@ -16,8 +17,13 @@ int count;
 double v_k; //velocity
 double t_s; //tempo campionamento time
 double w_k;
+double alpha;
 double vx;
 double vy;
+
+const double l=0.130; //Sarebbe 1.30 ma per una questione grafica mettiamo 0.130
+const double steering_factor = 18;
+const double d = 1.765;
 
 
 
@@ -27,9 +33,6 @@ bool odometryComputation(projectbergamascodigiusto::OdometryComputation::Request
 
     //INIT
 
-    double l=1.30;
-    double steering_factor = 18;
-    double d = 1.765;
 
     if((bool)req.changed){
         x_before=(double)req.x_init;
@@ -51,9 +54,14 @@ bool odometryComputation(projectbergamascodigiusto::OdometryComputation::Request
             vy=v_k*sin(theta_before);
             
             //Euler
-            x_comput=x_before+t_s*vx;
+            /*x_comput=x_before+t_s*vx;
             y_comput=y_before+t_s*vy;
-            theta_comput=theta_before+w_k*t_s;
+            theta_comput=theta_before+w_k*t_s;*/
+
+            //Runge-kutta
+            x_comput = x_before + v_k*t_s*cos(theta_before + (w_k*t_s)*0.5);
+            y_comput = y_before + v_k*t_s*sin(theta_before + (w_k*t_s)*0.5);
+            theta_comput = theta_before + w_k*t_s;
 
             ROS_INFO("[SERVER]  position: (x,y)=(%f,%f) theta=%f ",x_comput,y_comput,theta_comput);
             ROS_INFO("[SERVER] INFO: Ts=%f, w=%f, v=%f", t_s,w_k,v_k);
@@ -75,10 +83,38 @@ bool odometryComputation(projectbergamascodigiusto::OdometryComputation::Request
             break;
 
         case (2):     //Ackerman
-            /*t_s=(double)req.seconds-time_before;
-            vel = 0.5*((double)req.speedL + (double)req.speedR);
+            t_s=(double)req.seconds-time_before;
+            v_k = 0.5*((double)req.speedL + (double)req.speedR);
             alpha = (double)req.steer_sensor / steering_factor;
-            vx = vel*cos()*/
+            w_k = (v_k * tan(alpha)) / d;
+
+            vx = v_k*cos(alpha);
+            vy = v_k*sin(alpha);
+
+            //Euler
+            /*x_comput=x_before+t_s*vx;
+            y_comput=y_before+t_s*vy;
+            theta_comput=theta_before+w_k*t_s;*/
+
+            //Runge-Kutta
+            x_comput = x_before + v_k*t_s*cos(theta_before + (w_k*t_s)*0.5);
+            y_comput = y_before + v_k*t_s*sin(theta_before + (w_k*t_s)*0.5);
+            theta_comput = theta_before + w_k*t_s;
+
+            ROS_INFO("[SERVER]  position: (x,y)=(%f,%f) theta=%f ",x_comput,y_comput,theta_comput);
+            ROS_INFO("[SERVER] INFO: Ts=%f, w=%f, v=%f, alpha=%f", t_s,w_k,v_k, alpha);
+
+            res.x=x_comput;
+            res.y=y_comput;
+            res.steer_comput=theta_comput;
+            res.vx=vx;
+            res.vy=vy;
+            res.vt=w_k;
+
+            x_before=x_comput;
+            y_before=y_comput;
+            time_before=(double)req.seconds;
+            theta_before=theta_comput;
 
             break;
         
